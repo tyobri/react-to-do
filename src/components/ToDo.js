d8392993af713d90fddcd3fa37acfe9ed3ed7e64
Tyler <tyler.obrien1@gmail.com>
2018-04-10 11:22:16 -0700

    adding the ToDo list to react app

---
+++ src/components/ToDo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+	render() {
+		return {
+			<l1>A todo will go here </li>
+			<l1>A second todo here </li>
+		};
+	}
+}
+
+export default ToDo;
